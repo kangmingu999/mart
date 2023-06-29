77f12de5c39247ff7302561311aeaee063595915
rkdehdrn <rkdehdrn9@naver.com>
2023-06-29 11:20:10 +0900

    main

---
+++ src/mart/Main.java
@@ -0,0 +1,24 @@
+package mart;
+
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	int num;
+	while(true) {
+		System.out.println("1. 관리자");
+		System.out.println("2. 소비자");
+		System.out.println("3. 캐셔");
+		System.out.println("4. 나가기");
+		num = input.nextInt();
+		switch(num) {
+		case 1: break;
+		case 2: break;
+		case 3: break;
+		case 4: break;
+		
+	}
+}
+}
+}
