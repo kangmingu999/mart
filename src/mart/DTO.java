0798b53b4a4d3d943089b0d0bd03ec3816c8f293
rkdehdrn <rkdehdrn9@naver.com>
2023-06-29 11:28:13 +0900

    DTO

---
+++ src/mart/DTO.java
@@ -0,0 +1,24 @@
+package mart;
+
+public class DTO {
+	private int cash,num;
+	private String name;
+	public int getCash() {
+		return cash;
+	}
+	public void setCash(int cash) {
+		this.cash = cash;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
